52e80bab98440dc7b28d460163d7b70d4eedea03
Thomas Luca Pandolfo Altaffer <taltaffe@ucsc.edu>
2022-05-31 23:45:31 -0700

    Luca 5/31 - Knee rotation done and main progress

---
+++ FinalProject/FinalProject.X/build/default/production/_ext/2049610667/testFlex.o.d
@@ -0,0 +1,13 @@
+build/default/production/_ext/2049610667/testFlex.o: \
+ ../../Common/testFlex.c ../../Common/BOARD.h ../../Common/AD.h \
+ ../../Common/timers.h ../../Common/serial.h ../../Common/kneeRotation.h
+
+../../Common/BOARD.h:
+
+../../Common/AD.h:
+
+../../Common/timers.h:
+
+../../Common/serial.h:
+
+../../Common/kneeRotation.h:
